bfb0a934658acda16d1038a42849bdf831c9f410
AnyWayDude <zpaulz61@gmail.com>
2023-02-20 01:55:13 +0100

    add user

--- public/javascript/game.mjs
+++ public/javascript/game.mjs
@@ -1,7 +1,20 @@
+import { showMessageModal } from "./views/modal.mjs";
+
 const username = sessionStorage.getItem('username');
+
 
 if (!username) {
 	window.location.replace('/login');
 }
 
 const socket = io('', { query: { username } });
+
+socket.on('NEW_USER_ERROR', () => {
+	showMessageModal({
+		message: 'This name is already in use',
+		onclose: () => {
+			sessionStorage.clear('username');
+			window.location.replace('/login');
+		},
+	});
+});
--- src/socket/index.ts
+++ src/socket/index.ts
@@ -2,7 +2,24 @@ import { Server } from 'socket.io';
 import * as config from './config';
 
 export default (io: Server) => {
-	io.on('connection', socket => {
+	io.on('connection', async (socket) => {
 		const username = socket.handshake.query.username;
+		console.log(`${username}coected`);
+
+		const socketData = await io.fetchSockets();
+
+		const allUsers = socketData.map((el) => {
+			return el.handshake.query.username;
+		});
+
+		const userNameTaken = allUsers.filter((name) => name === username).length > 1;
+
+		if (userNameTaken) {
+			socket.emit('NEW_USER_ERROR')
+		}
+
+
+
 	});
+
 };
